0e46479ae98093d34ad4653cd89e408bea4a0a08
A-1 pressler <ohyes1356@outlook.com>
2022-12-07 21:37:10 -0800

    Added logic for handling snowballs.

--- GameCode/Game.java
+++ GameCode/Game.java
@@ -65,4 +65,91 @@ public class Game
 
         return moves;
     }
+
+    public boolean isLegalSnowBall(Point position, Point click, Tile[][] board)
+    {
+        List<Point> moves = snowBallPaths(position, "", 3, board);
+
+        for(int i = 0; i < moves.size(); i++)
+        {
+            if(moves.get(i).equals(click))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public List<Point> snowBallPaths(Point position, String direction, int bounds, Tile[][] board)
+    {
+        List<Point> paths = new ArrayList<Point>();
+
+        if(bounds == 0)
+        {
+            paths.add(position);
+
+            return paths;
+        }
+
+        switch(direction)
+        {
+            case "":
+            if(board[position.x][position.y].tileType.indexOf("u") != -1)
+            {
+                paths.add(new Point(position.x, position.y - 1));
+                paths.addAll(snowBallPaths(new Point(position.x, position.y - 1), "u", bounds - 1, board));
+            }
+            if(board[position.x][position.y].tileType.indexOf("l") != -1)
+            {
+                paths.add(new Point(position.x - 1, position.y));
+                paths.addAll(snowBallPaths(new Point(position.x - 1, position.y), "l", bounds - 1, board));
+            }
+            if(board[position.x][position.y].tileType.indexOf("r") != -1)
+            {
+                paths.add(new Point(position.x + 1, position.y));
+                paths.addAll(snowBallPaths(new Point(position.x + 1, position.y), "r", bounds - 1, board));
+            }
+            if(board[position.x][position.y].tileType.indexOf("d") != -1)
+            {
+                paths.add(new Point(position.x, position.y + 1));
+                paths.addAll(snowBallPaths(new Point(position.x, position.y + 1), "d", bounds - 1, board));
+            }
+            break;
+
+            case "u":
+            if(board[position.x][position.y].tileType.indexOf("u") != -1)
+            {
+                paths.add(new Point(position.x, position.y - 1));
+                paths.addAll(snowBallPaths(new Point(position.x, position.y - 1), "u", bounds - 1, board));
+            }
+            break;
+
+            case "l":
+            if(board[position.x][position.y].tileType.indexOf("l") != -1)
+            {
+                paths.add(new Point(position.x - 1, position.y));
+                paths.addAll(snowBallPaths(new Point(position.x - 1, position.y), "l", bounds - 1, board));
+            }
+            break;
+
+            case "r":
+            if(board[position.x][position.y].tileType.indexOf("r") != -1)
+            {
+                paths.add(new Point(position.x + 1, position.y));
+                paths.addAll(snowBallPaths(new Point(position.x + 1, position.y), "r", bounds - 1, board));
+            }
+            break;
+
+            case "d":
+            if(board[position.x][position.y].tileType.indexOf("d") != -1)
+            {
+                paths.add(new Point(position.x, position.y + 1));
+                paths.addAll(snowBallPaths(new Point(position.x, position.y + 1), "d", bounds - 1, board));
+            }
+            break;
+        }
+
+        return paths;
+    }
 }
